6d1bf4867eb0c4f4cbf04932ea39cee63e57bd71
Farmer Ed <63633329+Farmer-Eds-Shed@users.noreply.github.com>
2022-09-10 10:06:37 +0100

    Create AnimalSearchToolbarSlotProvider.alink.vue

---
+++ Animals/AnimalSearchToolbarSlotProvider.alink.vue
@@ -0,0 +1,3 @@
+<template alink-slot[net.symbioquine.farmos_asset_link.tb_item.v0.text_asset_search]="toolbar-item(weight: 50)">
+  <q-btn flat dense to="/find/asset/animal-search" icon="mdi-cow"></q-btn>
+</template>
