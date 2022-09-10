24732fc61a7bb875427c5b082415b0cb41f05e3d
Farmer Ed <63633329+Farmer-Eds-Shed@users.noreply.github.com>
2022-09-10 10:05:09 +0100

    Create AnimalSearcMethodSlotProvider.alink.vue

---
+++ Animals/AnimalSearcMethodSlotProvider.alink.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue';
+import { uuidv4 } from 'assetlink-plugin-api';
+
+const searchText = ref('');
+
+const emit = defineEmits(["update:searchRequest"]);
+
+const onSearchTextUpdated = (text) => {
+  emit('update:searchRequest', {
+    id: uuidv4(),
+    type: 'animal-search',
+    term: text,
+  });
+};
+</script>
+
+<template alink-slot[net.symbioquine.farmos_asset_link.asset_search.v0.text]="asset-search-method(weight: 50)">
+  <search-method
+    name="animal-search"
+    icon="mdi-cow">
+
+    <template #search-interface>
+      <div class="q-px-md">
+        <q-input autofocus v-model="searchText" @update:model-value="(evt) => onSearchTextUpdated(evt)" label="Search Animals" />
+      </div>
+    </template>
+
+  </search-method>
+
+</template>
