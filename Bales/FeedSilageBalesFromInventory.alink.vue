d1be3a73f2c627813695df6d7d2d11bee068c016
Farmer Ed <63633329+Farmer-Eds-Shed@users.noreply.github.com>
2022-11-20 09:54:56 +0000

    Update FeedSilageBalesFromInventory.alink.vue

--- Bales/FeedSilageBalesFromInventory.alink.vue
+++ Bales/FeedSilageBalesFromInventory.alink.vue
@@ -97,7 +97,7 @@ export default {
 
       action.showIf(({ asset }) => asset.attributes.status !== 'archived'
           // TODO: Implement a better predicate here...
-          && asset.attributes.name.toLowerCase().indexOf("Silage bales") !== -1);
+          && asset.attributes.name.toLowerCase().indexOf("Silage Bales") !== -1);
 
       const doActionWorkflow = async (asset) => {
         const baleCount = await assetLink.ui.dialog.custom(handle.thisPlugin, { asset });
